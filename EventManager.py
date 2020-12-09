4f3079049bb429e613a819ef4132d567c0439867
wrideout <william.rideout@gmail.com>
2020-12-08 21:19:54 -0500

    test msg contents

--- EventManager.py
+++ EventManager.py
@@ -55,7 +55,9 @@ def handle_event(event_type, event):
         # token and strip off the last two chars.
         message = message.split()[0]
         if sending_usr in message:
-            print('Skipping self plus or decrement')
+            print('++' in message)
+            print('Skipping self bump')
+            #karmaBot.chastise(inc=)
             return make_response('Got a self bump', 200)
         if increment_regex.match(message):
             karmaBot.increment(message[:-2], channel_id)
