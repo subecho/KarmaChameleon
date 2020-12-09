e16c4117f0dc508092f870104d6f64be8c788a8a
wrideout <william.rideout@gmail.com>
2020-12-08 21:35:36 -0500

    Prevent users from ++/-- themselves, fix default message code returned by handle_event.

--- EventManager.py
+++ EventManager.py
@@ -53,7 +53,7 @@ def handle_event(event_type, event):
         message = event_detail.get('text', '')
         print(message)
         if sending_usr in message:
-            print('Skipping self bump: %s' % '++' in message)
+            print('Skipping self bump.)
             karmaBot.chastise(('++' in message), channel_id)
             return make_response('Got a self bump', 200)
 
