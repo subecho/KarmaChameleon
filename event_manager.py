ff3bd38649c08d682da80e732131650217540256
wrideout <william.rideout@gmail.com>
2021-01-26 23:10:15 -0500

    add funcname

--- event_manager.py
+++ event_manager.py
@@ -34,7 +34,7 @@ logger = logging.getLogger('kc_event_manager')
 logger.setLevel(logging.DEBUG)
 file_handler = logging.FileHandler('karmachameleon.log')
 file_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s %(name)s[%(levelname)s]: %(message)s')
+formatter = logging.Formatter('%(asctime)s %(name)s[%(levelname)s]: (%(funcName)20s) %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
