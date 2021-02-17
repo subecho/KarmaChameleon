340e712d653d2af663bcb11f558275152dee8a8b
Will Rideout <william.rideout@gmail.com>
2021-02-17 10:05:42 -0500

    Add Ben to the list of authors

--- karma_chameleon.py
+++ karma_chameleon.py
@@ -1,5 +1,5 @@
 # Karma Chameleon
-# Copyright (C) 2021 Dustin Schoenbrun, Will Rideout
+# Copyright (C) 2021 Dustin Schoenbrun, Will Rideout, Ben Decato
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
