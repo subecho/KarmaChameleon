addd19b27009dad8bea5c435a72e2ce9ca968ae4
Will Rideout <william.rideout@gmail.com>
2021-05-09 21:01:04 -0400

    Update karma_chameleon/main.py
    Co-authored-by: Dustin Schoenbrun <subecho@users.noreply.github.com>

--- karma_chameleon/main.py
+++ karma_chameleon/main.py
@@ -152,7 +152,7 @@ def decrement(message: dict, say: Say) -> None:
 
     Arguments:
     message -- dictionary representation of the message which contains a karma operation
-    say -- method for printing back to the same channel from which the command was run
+    say -- The method for outputting the response to the channel from which the command was run.
     """
     rsp = app.decrement_karma(message)
     say(rsp)
